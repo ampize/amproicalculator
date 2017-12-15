c9f9badc9215be4aca4dc2813c6678eca4205df9
Alexandru Dobre <alexandru.dobre@webtales.fr>
2017-12-15 10:48:20 +0100

    WIP

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -10,57 +10,41 @@ use Symfony\Component\DomCrawler\Crawler as DomCrawler;
 class APIController extends BaseController
 {
 
-    public function getSWMetrics(Request $request)
-    {
-        $url = $request->input("url", null);
-        if (empty($url)) {
-            abort(400, "Missing required params");
+    protected $client=null;
+
+    protected function getClient(){
+        if(!$this->client){
+            $client=new \Google_Client();
+            $client->setApplicationName("AMPROICalculator");
+            $client->setScopes(implode(' ', array(
+                    \Google_Service_Sheets::SPREADSHEETS,\Google_Service_Slides::PRESENTATIONS)
+            ));
+            $client->setAuthConfig(realpath(__DIR__.'/../../../client_secret.json'));
+            $client->setAccessType('offline');
+            $credentialsPath=realpath(__DIR__.'/../../../credentials/credentials.json');
+            $accessToken = json_decode(file_get_contents($credentialsPath), true);
+            $client->setAccessToken($accessToken);
+            if ($client->isAccessTokenExpired()) {
+                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+                file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
+            }
+            $this->client=$client;
         }
-        $client = new GoutteClient();
-        $client->followRedirects();
-        $guzzleClient = new \GuzzleHttp\Client(array(
-            'curl' => array(
-                CURLOPT_SSL_VERIFYHOST => false,
-                CURLOPT_SSL_VERIFYPEER => false,
-            ),
-        ));
-        $client->setClient($guzzleClient);
-        $client->setHeader('User-Agent', "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)");
-        $crawler = $client->request('GET', 'https://www.similarweb.com/fr/website/'.$url);
-        $averageVisits=$crawler->filter(".engagementInfo-valueNumber")->first()->text();
-        $screenShotUrl=$crawler->filter(".stickyHeader-screenshot")->first()->attr("src");
-        $averagePages=$crawler->filter(".engagementInfo-value .engagementInfo-valueNumber")->eq(2)->text();
-        $multiplier=1;
-        if(strpos($averageVisits,"M")!==false){
-            $multiplier=1000000;
-        } else if(strpos($averageVisits,"K")!==false){
-            $multiplier=1000;
-        }
-        $averageVisits=floatval($averageVisits)*$multiplier;
-        return response()->json([
-            "averageVisits" => $averageVisits,
-            "averagePagesPerVisit" => str_replace('.',',',$averagePages),
-            "screenShotUrl" => $screenShotUrl,
-        ]);
+        return $this->client;
     }
 
-    public function getReport(Request $request)
-    {
-        $url = $request->input("url", null);
-        if (empty($url)) {
-            abort(400, "Missing required params");
-        }
-        $client = new GoutteClient();
-        $client->followRedirects();
+    protected function generateReport($url){
+        $gClient = new GoutteClient();
+        $gClient->followRedirects();
         $guzzleClient = new \GuzzleHttp\Client(array(
             'curl' => array(
                 CURLOPT_SSL_VERIFYHOST => false,
                 CURLOPT_SSL_VERIFYPEER => false,
             ),
         ));
-        $client->setClient($guzzleClient);
-        $client->setHeader('User-Agent', "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)");
-        $crawler = $client->request('GET', 'https://www.similarweb.com/fr/website/'.$url);
+        $gClient->setClient($guzzleClient);
+        $gClient->setHeader('User-Agent', "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)");
+        $crawler = $gClient->request('GET', 'https://www.similarweb.com/fr/website/'.$url);
         $averageVisits=$crawler->filter(".engagementInfo-valueNumber")->first()->text();
         $screenShotUrl=$crawler->filter(".stickyHeader-screenshot")->first()->attr("src");
         $averagePages=$crawler->filter(".engagementInfo-value .engagementInfo-valueNumber")->eq(2)->text();
@@ -85,20 +69,7 @@ class APIController extends BaseController
         if (empty($uniqueVisitors)||empty($averageVisits)||empty($averagePages)||empty($url)) {
             abort(400, "Missing required params");
         }
-        $client=new \Google_Client();
-        $client->setApplicationName("AMPROICalculator");
-        $client->setScopes(implode(' ', array(
-                \Google_Service_Sheets::SPREADSHEETS,\Google_Service_Slides::PRESENTATIONS)
-        ));
-        $client->setAuthConfig(realpath(__DIR__.'/../../../client_secret.json'));
-        $client->setAccessType('offline');
-        $credentialsPath=realpath(__DIR__.'/../../../credentials/credentials.json');
-        $accessToken = json_decode(file_get_contents($credentialsPath), true);
-        $client->setAccessToken($accessToken);
-        if ($client->isAccessTokenExpired()) {
-            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-            file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
-        }
+        $client=$this->getClient();
         $sheetsService = new \Google_Service_Sheets($client);
         $slidesService = new \Google_Service_Slides($client);
         $spreadSheetModelId = "1HqEblVk-6pCX8caa90zL6uVOYDepC792Mh76TI1OcXA";
@@ -231,48 +202,71 @@ class APIController extends BaseController
         ));
         $slidesService->presentations->batchUpdate($newSlidesId, $batchUpdateRequest);
         $pub=$driveService->revisions->update($newSlidesId,1,new \Google_Service_Drive_Revision(["published"=>true,"publishAuto"=>true]));
+        return $newSlidesId;
+    }
+
+    protected function hasReport($url){
+        $client=$this->getClient();
+        $driveService = new \Google_Service_Drive($client);
+        $files=$driveService->files->listFiles([
+            "q"=>"name='AMP ROI on ".$url."'"
+        ]);
+        if(!empty($files->files)&&!empty($files->files[0])){
+            return $files->files[0]->id;
+        }
+        return null;
+    }
+
+
+    public function getReport(Request $request)
+    {
+        $url = $request->input("url", null);
+        if (empty($url)) {
+            abort(400, "Missing required params");
+        }
+        $reportId=$this->hasReport($url);
+        if(!$reportId){
+            $reportId=$this->generateReport($url);
+        }
         return response()->json([
-            "id" => $newSlidesId,
+            "id" => $reportId,
             "success"=>true
 
         ]);
-//        $export = $driveService->files->export($newSlidesId, 'application/pdf', array(
-//            'alt' => 'media'));
-//        $content = $export->getBody()->getContents();
-//        return response($content)
-//            ->withHeaders([
-//                'Content-Type' => 'application/pdf',
-//            ]);
     }
+
+    public function downloadReportPDF(Request $request)
+    {
+        $url = $request->input("url", null);
+        if (empty($url)) {
+            abort(400, "Missing required params");
+        }
+        $reportId=$this->hasReport($url);
+        if(!$reportId){
+            $reportId=$this->generateReport($url);
+        }
+        $client=$this->getClient();
+        $driveService = new \Google_Service_Drive($client);
+        $export = $driveService->files->export($reportId, 'application/pdf', array(
+            'alt' => 'media'));
+        $content = $export->getBody()->getContents();
+        return response($content)
+            ->withHeaders([
+                'Content-Type' => 'application/pdf',
+            ]);
+    }
+
+
+
 
     public function testUpdate(Request $request)
     {
-        $client=new \Google_Client();
-        $client->setApplicationName("AMPROICalculator");
-        $client->setScopes(implode(' ', array(
-                \Google_Service_Sheets::SPREADSHEETS,\Google_Service_Slides::PRESENTATIONS)
-        ));
-        $client->setAuthConfig(realpath(__DIR__.'/../../../client_secret.json'));
-        $client->setAccessType('offline');
-        $credentialsPath=realpath(__DIR__.'/../../../credentials/credentials.json');
-        $accessToken = json_decode(file_get_contents($credentialsPath), true);
-        $client->setAccessToken($accessToken);
-        if ($client->isAccessTokenExpired()) {
-            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-            file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
+        $url = $request->input("url", null);
+        if (empty($url)) {
+            abort(400, "Missing required params");
         }
-        $sheetsService = new \Google_Service_Sheets($client);
-        $slidesService = new \Google_Service_Slides($client);
-        $driveService = new \Google_Service_Drive($client);
-        $slidesId="1ACrSlNHwX-S-wPG5NP-ZHjRDRR4etEdlUd-9f0AXRDo";
-        $spreadSheetModelId = "1HqEblVk-6pCX8caa90zL6uVOYDepC792Mh76TI1OcXA";
-        $rezRanges=[
-            "'Cash Flow Table (Risk-Adjusted)'!G5",
-            "'Cash Flow Table (Risk-Adjusted)'!G32",
-            "'Cash Flow Table (Risk-Adjusted)'!G31",
-        ];
-        $values=$sheetsService->spreadsheets_values->batchGet($spreadSheetModelId,["ranges"=>$rezRanges]);
-        var_dump($values->getValueRanges()[1]->getValues()[0][0]);
-        die;
+        var_dump( $this->hasReport($url));
+        die("test");
+
     }
 }
