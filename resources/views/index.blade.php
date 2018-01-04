43d843c2eae273d80632a48bb7fd91256dbbd167
Didier Fanchon <didier.fanchon@webtales.fr>
2018-01-04 10:18:39 +0100

    WIP

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -126,11 +126,17 @@
                 <p class="intro center">Learn what you could expect from shifting to AMP/PWA and using AMPize with our deep learning based ROI calculator</p>
                 <div id="embed-api-auth-container"></div>
                 <div id="view-selector-container"></div>
-                <a href="" ng-click="ARC.authorizeGa()" ng-disabled="ARC.isLoading" ng-show="!ARC.hasAnalytics">Get your real site metrics from Google Analytics</a>
-                <input type="url" class="input-url" ng-model="ARC.url" placeholder="http://" required>
-                <button class="submit-btn" ng-click="ARC.submit()" ng-disabled="ARC.isLoading">Estimate your ROI</button>
-                <button class="submit-btn" ng-click="ARC.submitWithAnalytics()" ng-disabled="ARC.isLoading" ng-show="ARC.hasAnalytics">Estimate your ROI from real analytics</button>
-                <iframe ng-if="ARC.hasPreview" ng-src="{{ARC.previewUrl}}" frameborder="0" width="480" height="299"></iframe>
+                <div>
+                    <a href="" ng-click="ARC.authorizeGa()" ng-disabled="ARC.isLoading" ng-show="!ARC.hasAnalytics">Get your real site metrics from Google Analytics</a>
+                </div>
+                <div>
+                    <input type="url" class="input-url" ng-model="ARC.url" placeholder="http://" required>
+                    <button class="submit-btn" ng-click="ARC.submit()" ng-disabled="ARC.isLoading">Estimate your ROI</button>
+                    <button class="submit-btn" ng-click="ARC.submitWithAnalytics()" ng-disabled="ARC.isLoading" ng-show="ARC.hasAnalytics">Estimate your ROI from real analytics</button>
+                </div>
+                <div>
+                    <iframe ng-if="ARC.hasPreview" ng-src="{{ARC.previewUrl}}" frameborder="0" width="480" height="299"></iframe>
+                </div>
         </div>
     </div>
 </div>
